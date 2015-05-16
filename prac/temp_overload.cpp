5b55866350d2b0924789bc37bf84c62e759fe826
nikhil <nkmittal4994@gmail.com>
2015-05-17 00:01:09 +0530

    overloading with template.

--- prac/temp_overload.cpp
+++ prac/temp_overload.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 using namespace std;
 
-// TODO: Overload + template
+template <class t>
+class complex
+{
+	t real;
+	t img;
+public:
+	complex()
+	{
+		real = img = 0.0;
+	}
+	complex(t x,t y) : real(x),img(y) {}
+	complex operator + (complex c);
 
-template <typename t>
+	void out()
+	{
+		cout << real << " + i"<< img;
+	}
+};
 
-
+template <class t>
+complex <t> complex <t>::operator +(complex <t> c)
+{
+	complex <t> temp;
+	temp.real = real + c.real;
+	temp.img = img + c.img;
+	return temp;
+}
 int main()
 {
-	
-	return 0;
+	complex <int> c1,c3(4,8),c2(3,4);
+	c1 = c3 + c2;
+	c1.out();
 }
